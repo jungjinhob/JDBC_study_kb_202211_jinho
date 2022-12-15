1ec1a3cd70b3bc8962b8ae823d0bc0d03806e4ba
jungjinhob <hahahyogo@gmail.com>
2022-12-15 10:17:24 +0900

    12/15 rrr

--- src/main/java/com/study/jdbc/main/JdbcTest1.java
+++ src/main/java/com/study/jdbc/main/JdbcTest1.java
@@ -2,6 +2,9 @@ package main.java.com.study.jdbc.main;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import main.java.com.study.jdbc.util.DBConnection;
 
@@ -11,8 +14,26 @@ public class JdbcTest1 {
 
 //		System.out.println(Driver.class.getName());
 //동일	System.out.println("com.mysql.cj.jdbc.Driver");  
-		Connection connection = DBConnection.getInstance().getConnection();
-		System.out.println(connection);
+		Connection connection = DBConnection.getInstance().getConnection(); //싱글톤 (데이터베이스 연결)
+		
+		String sql = "select * from score_mst"; //쿼리문 
+		
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(sql); //연결된 db안에 sql 작성. 쿼리를 들고만 있는중.
+			ResultSet rs = pstmt.executeQuery(); // 쿼리 실행. 리턴이 resultset(결과를 갖고 있는 set)
+			//executeupdate 
+			System.out.println("id\tname\t\tscore");
+			
+			while(rs.next()) { //rs.next가 false될때까지 반복.
+										// sql변수에 저장되있는 데이터를 한줄씩 (rs) 꺼내서 (push 즉, 없어짐) 출력하다가 줄이 비어버리면 그만(false)
+				System.out.println("id : " + rs.getInt(1) //1번칼럼 ~3번칼럼 (컬럼은 1번부터 시작)
+				+ "\t name : " + rs.getString(2) 
+				+ "\t score : " + rs.getInt(3));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
--- src/main/java/com/study/jdbc/util/DBConnection.java
+++ src/main/java/com/study/jdbc/util/DBConnection.java
@@ -29,11 +29,12 @@ public class DBConnection {
 		try {
 			Class.forName(Driver.class.getName()); //객체 생성
 			System.out.println("데이터베이스 드라이버 로딩 성공!");
-			url = "jdbc:mysql://localhost:3306/subquery_study";
+			url = "jdbc:mysql://localhost:3306/subquery_study"; //subquery ... -> 스키마명.
 			username = "root";
 			password = "root";
 			
-			connection = DriverManager.getConnection(url, username, password); 
+			connection = DriverManager.getConnection(url, username, password);  //커넥션 객체 생성.
+			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			System.out.println("드라이버 로딩 실패 !");
@@ -41,7 +42,7 @@ public class DBConnection {
 			e.printStackTrace();
 			System.out.println("데이터베이스 연결 실패! ");
 		}
-		return connection;
+		return connection; //연결된 객체.
 	}
 	
 	
